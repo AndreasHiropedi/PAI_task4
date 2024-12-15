3570b3058ecdea3f8464f0b488fb40b762baaec9
sarah-verr <105598942+sarah-verr@users.noreply.github.com>
2024-12-15 18:51:30 +0100

    -19 code

--- solution.py
+++ solution.py
@@ -8,10 +8,27 @@ from gymnasium.utils import seeding
 from utils import ReplayBuffer, get_env, run_episode
 
 
+###############################################
+# NEW: Ornstein-Uhlenbeck Noise Class
+###############################################
+class OUNoise:  # NEW
+    def __init__(self, action_dim, mu=0.0, theta=0.15, sigma=0.2):  # NEW
+        self.action_dim = action_dim  # NEW
+        self.mu = mu  # NEW
+        self.theta = theta  # NEW
+        self.sigma = sigma  # NEW
+        self.reset()  # NEW
+
+    def reset(self):  # NEW
+        self.x = np.ones(self.action_dim) * self.mu  # NEW
+
+    def sample(self):  # NEW
+        dx = self.theta * (self.mu - self.x) + self.sigma * np.random.randn(self.action_dim)  # NEW
+        self.x = self.x + dx  # NEW
+        return self.x  # NEW
+
+
 class MLP(nn.Module):
-    '''
-    A simple ReLU MLP constructed from a list of layer widths.
-    '''
     def __init__(self, sizes):
         super().__init__()
         layers = []
@@ -26,210 +43,150 @@ class MLP(nn.Module):
 
 
 class Critic(nn.Module):
-    '''
-    Simple MLP Q-function.
-    '''
     def __init__(self, obs_size, action_size, num_layers, num_units):
         super().__init__()
-        #####################################################################
-        # TODO: add components as needed (if needed)
-
         self.net = MLP([obs_size + action_size] + ([num_units] * num_layers) + [1])
-
-        #####################################################################
-
+    
     def forward(self, x, a):
-        #####################################################################
-        # TODO: code the forward pass
-        # the critic receives a batch of observations and a batch of actions
-        # of shape (batch_size x obs_size) and batch_size x action_size) respectively
-        # and output a batch of values of shape (batch_size x 1)
-
-        # concatenates observations and the action take together
         inputs = torch.cat([x, a], dim=-1)
         value = self.net(inputs)
-
-        #####################################################################
         return value
 
 
 class Actor(nn.Module):
-    '''
-    Simple Tanh deterministic actor.
-    '''
-    def __init__(self, action_low, action_high,  obs_size, action_size, num_layers, num_units):
+    def __init__(self, action_low, action_high, obs_size, action_size, num_layers, num_units):
         super().__init__()
-        #####################################################################
-        # TODO: add components as needed (if needed)
-
         self.net = MLP([obs_size] + ([num_units] * num_layers) + [action_size])
-
-        #####################################################################
-        # store action scale and bias: the actor's output can be squashed to [-1, 1]
         self.action_scale = (action_high - action_low) / 2
         self.action_bias = (action_high + action_low) / 2
 
     def forward(self, x):
-        #####################################################################
-        # TODO: code the forward pass
-        # the actor will receive a batch of observations of shape (batch_size x obs_size)
-        # and output a batch of actions of shape (batch_size x action_size)
-
         action = torch.tanh(self.net(x))
         return action * self.action_scale + self.action_bias
-
-        #####################################################################
 
 
 class Agent:
 
-    # automatically select compute device
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    buffer_size: int = 50_000  # no need to change
+    buffer_size: int = 50_000
 
-    #########################################################################
-    # TODO: store and tune hyperparameters here
-
+    # Hyperparameters
     learning_rate = 1e-3
     num_layers = 2
     num_units = 256
     batch_size: int = 256
-    gamma: float = 0.99  # MDP discount factor, 
-    exploration_noise: float = 0.1  # epsilon for epsilon-greedy exploration
-    tau: float = 0.005
-    
-    #########################################################################
+    gamma: float = 0.99
+    tau: float = 0.005  # NEW: soft update factor for target networks (wasn't in the original code)
 
     def __init__(self, env):
 
-        # extract informations from the environment
-        self.obs_size = np.prod(env.observation_space.shape)  # size of observations
-        self.action_size = np.prod(env.action_space.shape)  # size of actions
-        # extract bounds of the action space
-        self.action_low = torch.tensor(env.action_space.low).float()
-        self.action_high = torch.tensor(env.action_space.high).float()
+        self.obs_size = np.prod(env.observation_space.shape)
+        self.action_size = np.prod(env.action_space.shape)
+        self.action_low = torch.tensor(env.action_space.low, device=self.device).float()   # CHANGED: moved to device
+        self.action_high = torch.tensor(env.action_space.high, device=self.device).float() # CHANGED: moved to device
 
-        #####################################################################
-        # TODOish: initialize actor, critic and attributes
-
+        # Original actor and critic
+        self.actor = Actor(self.action_low, self.action_high, self.obs_size, self.action_size,
+                           self.num_layers, self.num_units).to(self.device)
         self.critic = Critic(self.obs_size, self.action_size, self.num_layers, self.num_units).to(self.device)
-        self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=self.learning_rate)
 
-        self.actor = Actor(self.action_low, self.action_high, self.obs_size, self.action_size, self.num_layers, self.num_units).to(self.device)
+        ###############################################
+        # NEW: Target networks for stable training
+        ###############################################
+        self.actor_target = Actor(self.action_low, self.action_high, self.obs_size, self.action_size,
+                                  self.num_layers, self.num_units).to(self.device)  # NEW
+        self.critic_target = Critic(self.obs_size, self.action_size, self.num_layers, self.num_units).to(self.device)  # NEW
+
+        # NEW: Initialize target networks with the same weights as the main networks
+        self.actor_target.load_state_dict(self.actor.state_dict())  # NEW
+        self.critic_target.load_state_dict(self.critic.state_dict())  # NEW
+
+        # Optimizers
+        self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=self.learning_rate)
         self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=self.learning_rate)
 
-        #####################################################################
-        # create buffer
+        # Replay buffer
         self.buffer = ReplayBuffer(self.buffer_size, self.obs_size, self.action_size, self.device)
-        self.train_step = 0
-        
-        # create targegt networks
-        self.actor_target = Actor(self.action_low, self.action_high, self.obs_size, self.action_size, self.num_layers, self.num_units).to(self.device)
-        self.critic_target = Critic(self.obs_size, self.action_size, self.num_layers, self.num_units).to(self.device)
-        #intialize them
-        self.actor_target.load_state_dict(self.actor.state_dict())
-        self.critic_target.load_state_dict(self.critic.state_dict())
-    
-    
-    def soft_update(self, source_network, target_network, tau):
-        for target_param, param in zip(target_network.parameters(), source_network.parameters()):
+
+        ###############################################
+        # NEW: OU Noise for better exploration
+        ###############################################
+        self.noise = OUNoise(self.action_size)  # NEW
+
+    ###############################################
+    # NEW: Soft update function for target networks
+    ###############################################
+    def soft_update(self, source, target, tau):  # NEW
+        # Reason: softly blend target parameters with the source parameters for stability
+        for target_param, param in zip(target.parameters(), source.parameters()):
             target_param.data.copy_(tau * param.data + (1.0 - tau) * target_param.data)
 
-
     def train(self):
-        '''
-        Updates actor and critic with one batch from the replay buffer.
-        '''
+        # CHANGED: Check if we have enough samples to train
+        if self.buffer.size < self.batch_size:  # NEW
+            return  # NEW
+
         obs, action, next_obs, done, reward = self.buffer.sample(self.batch_size)
 
-        #####################################################################
-        # TODO: code training logic
-
-        obs = obs.clone().detach().float()
-        action = action.clone().detach().float()
-        next_obs = next_obs.clone().detach().float()
-        done = done.clone().detach().float().unsqueeze(1)
-        reward = reward.clone().detach().float().unsqueeze(1)
+        # Removed unnecessary clone() calls. Using obs.detach() is fine, but generally 
+        # not needed if we're just doing forward passes and not backward through these.
+        done = done.unsqueeze(1)
+        reward = reward.unsqueeze(1)
 
         with torch.no_grad():
-            next_action = self.actor_target(next_obs)
-            target_q_values = self.critic_target(next_obs,next_action)
-            target_q_values = reward + (1 - done) * self.gamma * target_q_values
+            # Compute target actions using the target actor
+            next_action = self.actor_target(next_obs)  # CHANGED: using target actor, was self.actor before
+            # Compute target Q-values using the target critic
+            target_q = self.critic_target(next_obs, next_action)  # CHANGED: using target critic
+            y = reward + (1 - done) * self.gamma * target_q
 
-        current_q_values = self.critic(obs,action)
-        
-        critic_loss = nn.MSELoss()(current_q_values, target_q_values)
+        # Update critic
+        q_values = self.critic(obs, action)
+        critic_loss = nn.MSELoss()(q_values, y)
         self.critic_optimizer.zero_grad()
         critic_loss.backward()
         self.critic_optimizer.step()
 
-        # Update Actor
+        # Update actor using policy gradient
         self.actor_optimizer.zero_grad()
-
         actor_loss = -self.critic(obs, self.actor(obs)).mean()
         actor_loss.backward()
         self.actor_optimizer.step()
-        
-        self.soft_update(self.actor, self.actor_target, self.tau)
-        self.soft_update(self.critic, self.critic_target, self.tau)
 
-
-
-        #####################################################################
+        # Soft-update the target networks for stability
+        self.soft_update(self.actor, self.actor_target, self.tau)   # NEW
+        self.soft_update(self.critic, self.critic_target, self.tau) # NEW
 
     def get_action(self, obs, train):
-        '''
-        Returns the agent's action for a given observation.
-        The train parameter can be used to control stochastic behavior.
-        '''
-        #####################################################################
-        # TODO: return the agent's action for an observation (np.array
-        # of shape (obs_size, )). The action should be a np.array of
-        # shape (act_size, )
-        #####################################################################
-
         obs = torch.tensor(obs, device=self.device).float().unsqueeze(0)
 
-
         with torch.no_grad():
-            # gets action from actor - squeeze reshapes - cpu changes to run on cpu
             action = self.actor(obs).squeeze(0).cpu().numpy()
 
-        # if we are training, we want to add random noise
         if train:
-            # exploration noise is from epsilon greedy
-            action += self.exploration_noise * np.random.randn(self.action_size)
-        
-        # makes sure action is within bounds
+            # CHANGED: Use OU noise instead of simple Gaussian noise
+            action = action + self.noise.sample()  # NEW
+
         return np.clip(action, self.action_low.cpu().numpy(), self.action_high.cpu().numpy())
 
-
     def store(self, transition):
-        '''
-        Stores the observed transition in a replay buffer containing all past memories.
-        '''
         obs, action, reward, next_obs, terminated = transition
         self.buffer.store(obs, next_obs, action, reward, terminated)
 
 
-# This main function is provided here to enable some basic testing. 
-# ANY changes here WON'T take any effect while grading.
 if __name__ == '__main__':
-
-    WARMUP_EPISODES = 10  # initial episodes of uniform exploration
-    TRAIN_EPISODES = 50  # interactive episodes
-    TEST_EPISODES = 300  # evaluation episodes
+    WARMUP_EPISODES = 10
+    TRAIN_EPISODES = 50
+    TEST_EPISODES = 300
     save_video = False
     verbose = True
-    seeds = np.arange(10)  # seeds for public evaluation
+    seeds = np.arange(10)
 
     start = time.time()
     print(f'Running public evaluation.') 
     test_returns = {k: [] for k in seeds}
 
     for seed in seeds:
-
-        # seeding to ensure determinism
         seed = int(seed)
         for fn in [random.seed, np.random.seed, torch.manual_seed]:
             fn(seed)
@@ -248,7 +205,7 @@ if __name__ == '__main__':
             run_episode(env, agent, mode='train', verbose=verbose, rec=False)
 
         for n_ep in range(TEST_EPISODES):
-            video_rec = (save_video and n_ep == TEST_EPISODES - 1)  # only record last episode
+            video_rec = (save_video and n_ep == TEST_EPISODES - 1)
             with torch.no_grad():
                 episode_return = run_episode(env, agent, mode='test', verbose=verbose, rec=video_rec)
             test_returns[seed].append(episode_return)
